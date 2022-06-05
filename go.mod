a643a857bf754d124fcf32fc3fc56c944f173c8d
Vinicius Coelho <vinicoelhose@gmail.com>
2022-06-04 21:19:30 -0300

    Add API routes
    The gorilla-mux library was also added here to handle the routes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module exercise
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.1
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.7.1
+)
---
+++ src/router/routes.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"exercise/src/router/routes"
+	"exercise/src/types"
+)
+
+func CreateRoutes(cd types.CanvasDrawing) *mux.Router {
+	r := mux.NewRouter()
+
+	r.Path("/canvas").
+		Methods(http.MethodGet).
+		Name("DrawCanvas").
+		Handler(routes.NewDrawCanvasHandler(cd))
+	r.Path("/canvas").
+		Methods(http.MethodPost).
+		Name("ReadCanvas").
+		Handler(routes.NewReadCanvasHandler(cd))
+
+	return r
+}
